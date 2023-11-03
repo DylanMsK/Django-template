d6638cd44d2407b224d7a48ee3d865adbd7d6bfc
minsu kang <kms920612@gmail.com>
2023-11-03 21:53:20 +0900

    custom middleware 끝

--- config/middlewares/custom_middleware.py
+++ config/middlewares/custom_middleware.py
@@ -1,11 +1,6 @@
-import uuid
-from time import process_time_ns
+from time import process_time
 
-from django.conf import settings
 from rest_framework.request import Request
-
-
-AUTH_URL = getattr(settings, "AUTH_URL", None)
 
 
 class CustomMiddleware(object):
@@ -13,20 +8,10 @@ class CustomMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request: Request):
-        request.id = uuid.uuid4().hex
-
-        start_time = process_time_ns()
+        start_time = process_time()
         response = self.get_response(request)
-        end_time = process_time_ns()
+        end_time = process_time()
 
-        response["X-Runtime"] = (end_time - start_time) // 1000
+        response["X-Runtime"] = round((end_time - start_time) * 1000)
         response["X-Request-ID"] = request.id
         return response
-
-    # def process_exception(self, request, exception):
-    #     if isinstance(exception, PermissionDenied):
-    #         return HttpResponse(
-    #             str(exception), status=status.HTTP_401_UNAUTHORIZED
-    #         )  # PermissionDenied가 발생하면 401 상태 코드로 응답
-
-    #     return None
