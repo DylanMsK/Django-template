0be8df3fec13fa496006ead763e340d6d00b1d80
minsu kang <kms920612@gmail.com>
2023-11-05 00:30:01 +0900

    custom exception 테스트

--- core/views.py
+++ core/views.py
@@ -1,20 +1,21 @@
-from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from core.exceptions import CustomNotFound
+
 
 @api_view(["GET"])
 def sample_error(request):
-    # raise NotFound()
     if 5 / 0:
         return Response({})
 
 
 class SampleGetViewset(APIView):
     def get(self, request):
-        print(request.user)
+        print(request.user.id)
+        raise CustomNotFound()
         return Response({"status": "ok"})
 
 
@@ -22,3 +23,7 @@ class SamplePostViewset(APIView):
     def post(self, request):
         data = JSONParser().parse(request)
         return Response(data)
+
+
+class SampleFormDataViewset(APIView):
+    pass
