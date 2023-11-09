7e6c5e66752c4da70aa58a7ed811bf0ecaf38541
dylanmsk <kms920612@gmail.com>
2023-11-09 23:40:42 +0900

    develop docker compose 실행 스크립트 작성

---
+++ scripts/stop-dev.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if command -v docker-compose > /dev/null 2>&1 ; then
+    docker-compose -f deploy/docker-compose.yml -f deploy/docker-compose.dev.yml --project-directory . down
+else
+    # docker-compose 명령을 사용할 수 없는 경우, docker compose 명령을 시도합니다.
+    if command -v docker compose > /dev/null 2>&1 ; then
+        docker compose -f deploy/docker-compose.yml -f deploy/docker-compose.dev.yml --project-directory . down
+    else
+        echo "오류: 이 시스템에서는 'docker-compose'와 'docker compose' 명령어 중 어느 하나도 사용할 수 없습니다."
+        exit 1
+    fi
+fi
